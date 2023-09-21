e6f37e7332384f53f19b1dcbe36981b9513de04a
Docta Rhyf <drrhyf@gmail.com>
2023-09-21 18:21:34 +0200

    starting koopf

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,19 +17,7 @@ import {
 export default function Home({}) {
   return (
     <div className="">
-      <div className="w-72">
-        <Select label="Select Version">
-          <Option>
-            {" "}
-            <Square3Stack3DIcon color="blue" />
-            Material Tailwind HTML
-          </Option>
-          <Option>Material Tailwind React</Option>
-          <Option>Material Tailwind Vue</Option>
-          <Option>Material Tailwind Angular</Option>
-          <Option>Material Tailwind Svelte</Option>
-        </Select>
-      </div>
+      <div className="w-72">KOOPF final ...</div>
     </div>
   );
 }
