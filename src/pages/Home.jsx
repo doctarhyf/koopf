6c662f21224694a88b877c49a68d4f62dbec3372
Docta Rhyf <drrhyf@gmail.com>
2023-10-11 15:58:43 +0200

    verc versi

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,6 +18,7 @@ export default function Home({}) {
   return (
     <div className="">
       <div className="w-72">KOOPF final ...</div>
+      <div>This is some vercel shit ...</div>
       <Button>Cool</Button>
     </div>
   );
